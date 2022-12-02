957c5dcb3a34ddd05cf7efdafad2b522ecfdc7bf
EliottMMP <102738023+EliottMMP@users.noreply.github.com>
2022-12-02 12:33:06 +0100

    Update programme_1.py

--- programme_1.py
+++ programme_1.py
@@ -3,6 +3,7 @@
 
 #importation des bibliothèques
 import networkx as nx
+import random
 
 def creer_labyrinthe(nombre_colonnes, nombre_lignes, degradation):
     """ Produit un labyrinthe au format d’un Graphe NetworkX
@@ -10,13 +11,26 @@ def creer_labyrinthe(nombre_colonnes, nombre_lignes, degradation):
     G = nx.Graph()
     nb_sommet = nombre_colonnes*nombre_lignes
     
-    
     for index in range(nb_sommet):
         G.add_node(index)
-        
+        nb_ligne = index//nombre_colonnes
+        index_min = nb_ligne*nombre_colonnes
+        index_max = index_min + nombre_colonnes - 1
+        voisins = []
+        if index - 1 >= index_min:
+            voisins.append(index - 1)
+        if index + 1 <= index_max: 
+            voisins.append(index+1)
+        if index - nombre_colonnes > 0: 
+            voisins.append(index - nombre_colonnes)
+        if index + nombre_colonnes < nb_sommet:
+            voisins.append(index + nombre_colonnes)
+        for voisin in voisins:
+            if random.randint(0,1) == 0:
+                G.add_edge(index, voisin)
     return G
     
-    
+
 if __name__ == "__main__":
     # Lance le test de la fonction creer_labyrinthe()
     Labyrinthe = creer_labyrinthe(4, 3, 30)
